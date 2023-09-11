017d28e8c32a757a95233454336fea72bdb2eeff
sakshamkumr <122541184+sakshamkumr@users.noreply.github.com>
2023-09-11 23:21:42 +0530

    Add files via upload

---
+++ minimum.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include<climits>
+using namespace std;
+    int main(){
+    int arr[5] = {1,2,3,4,5};
+    int min = INT_MAX;
+    for(int i=0;i<5;i++){
+        min = min(min,arr[i]);
+     }
+    cout << min;
+    return 0;
+}
